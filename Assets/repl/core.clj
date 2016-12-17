a0d269534a9974546377fd4426122c95d1e3f936
selfsame <josphe.parker.1@gmail.com>
2016-12-17 17:39:13 -0500

    repl history

--- Assets/repl/core.clj
+++ Assets/repl/core.clj
@@ -4,6 +4,8 @@
   (:import
     [UnityEngine Input KeyCode]))
 
+(def repl-env (atom (arcadia.repl/env-map)))
+
 (def prompt  (atom 6))
 (def histidx (atom 0))
 (def ns-str  (atom "user=>"))
@@ -30,15 +32,31 @@
 (defn ->input-field [o] (cmpt o UnityEngine.UI.InputField))
 
 
-(def repl-env (atom (arcadia.repl/env-map)))
 
 (defn clear [input]
   (set! (.text input) @ns-str)
   (reset! prompt (count @ns-str)))
 
+(defn history! [input n]
+  (let [idx (+ @histidx (- n))
+        histcount (count @history)]
+    (log "history!" idx (subs (.text input) 0 @prompt))
+  (if (< -1 idx histcount)
+    (do 
+      (reset! histidx idx)
+      (set! (.text input)
+        (str (subs (.text input) 0 @prompt)
+             (nth @history (- (dec histcount) idx))))
+      (set! (.caretPosition input) (count (.text input))))
+    (set! (.caretPosition input) (count (.text input))))))
+
 (defn send-input [input]
   (let [form (subs (.text input) @prompt)
+        trimmed-form (subs form 0 (dec (count form)))
         {:keys [result env]} (arcadia.repl/repl-eval-print @repl-env form)]
+    (when-not (#{"" (last @history)} trimmed-form)
+      (swap! history conj trimmed-form))
+    (reset! histidx -1)
     (reset! repl-env env)
     (set! (.text input) 
           (str (.text input) result "\n" (ns-name (:*ns* @repl-env)) "=>"))
@@ -48,6 +66,9 @@
 (defn check-input [o]
   (let [input (->input-field o)]
     (cond 
+      (key-down? :up)   (history! input -1)
+      (key-down? :down) (history! input  1)
+
       (and (key-down? "c")
            (key? "left alt"))
       (clear input)
